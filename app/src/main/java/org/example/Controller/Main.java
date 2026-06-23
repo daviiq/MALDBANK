2ecfb0714df0f1a4cc1f01601519225359587431
Lucas Luiz Guesser <lucas.lg17@aluno.ifsc.edu.br>
2026-06-23 20:42:16 -0300

    Implementação da Classe Investimento.

--- app/src/main/java/org/example/Controller/Main.java
+++ app/src/main/java/org/example/Controller/Main.java
@@ -43,76 +43,96 @@ public class Main {
 
         usuario.imprimirDados();
 
-        System.out.println("Escolha qual tipo de conta você quer: ");
-        System.out.println("1 - Conta Corrente.");
-        System.out.println("2 - Conta Investimento.");
-        System.out.println("3 - Conta Poupança.");
-        System.out.println("4 - Conta Salário.");
+        while (true) {
+            System.out.println("Escolha qual tipo de conta você quer: ");
+            System.out.println("1 - Conta Corrente.");
+            System.out.println("2 - Conta Investimento.");
+            System.out.println("3 - Conta Poupança.");
+            System.out.println("4 - Conta Salário.");
+            System.out.println("0 - Sair.");
 
-        int escolhaConta = scanner.nextInt();
+            int escolhaConta = scanner.nextInt();
 
-        switch (escolhaConta) {
-            case 2:
-                Random random = new Random();
-                StringBuilder numeroContaRandom = new StringBuilder();
-                int quantidadeDigitos = 9;
+            switch (escolhaConta) {
+                case 0:
+                    System.out.println("Saindo...");
+                    return;
+                case 2:
+                    Random random = new Random();
+                    StringBuilder numeroContaRandom = new StringBuilder();
+                    int quantidadeDigitos = 9;
 
-                for (int i = 0; i < quantidadeDigitos; i++) {
-                    int digito = random.nextInt(10);
-                    numeroContaRandom.append(digito);
-                }
+                    for (int i = 0; i < quantidadeDigitos; i++) {
+                        int digito = random.nextInt(10);
+                        numeroContaRandom.append(digito);
+                    }
 
-                String numeroConta = numeroContaRandom.toString();
+                    String numeroConta = numeroContaRandom.toString();
 
-                ContaInvestimento contaInvestimento = new ContaInvestimento(usuario.getNome(), usuario, numeroConta);
+                    ContaInvestimento contaInvestimento = new ContaInvestimento(usuario.getNome(), usuario, numeroConta);
 
-                System.out.println("Digite 0 para sair.");
-                System.out.println("Escolha as opções: ");
-                System.out.println("1 - Depositar");
-                System.out.println("2 - Sacar.");
-                System.out.println("3 - Investir.");
+                    boolean voltar = false;
+                    while (!voltar) {
+                        System.out.println("Digite 0 para voltar.");
+                        System.out.println("Escolha as opções: ");
+                        System.out.println("1 - Depositar");
+                        System.out.println("2 - Sacar.");
+                        System.out.println("3 - Investir.");
 
-                int opcoesContaInvestimento = scanner.nextInt();
+                        int opcoesContaInvestimento = scanner.nextInt();
 
-                switch (opcoesContaInvestimento) {
-                    case 1:
-                        System.out.println("Seu saldo é de R$ " + contaInvestimento.getSaldo());
-                        System.out.println("Quanto deseja depositar?: ");
-                        double deposito = scanner.nextDouble();
-                        contaInvestimento.depositar(deposito);
-                        System.out.println("Você depositou R$ " + deposito + " e tem R$ " + contaInvestimento.getSaldo()
-                                + " de saldo.");
-                        return;
-                    case 2:
-                        System.out.println("Seu saldo é de R$ " + contaInvestimento.getSaldo());
-                        System.out.println("Quanto deseja sacar?: ");
-                        double saque = scanner.nextDouble();
-                        contaInvestimento.sacar(saque);
-                        System.out.println("Você sacou R$ " + saque + " e tem R$ " + contaInvestimento.getSaldo()
-                                + " de saldo.");
-                    case 3:
-                        System.out.println("Escolha os tipos de investimento: ");
-                        System.out.println("1 - CDB.");
-                        System.out.println("2 - Tesouro Direto.");
-                        System.out.println("3 - IBV.");
-                        System.out.println("4 - FCI.");
-                        System.out.println("5 - LCI.");
-
-                        int escolhaInvestimento = scanner.nextInt();
-
-                        switch  (escolhaInvestimento) {
+                        switch (opcoesContaInvestimento) {
+                            case 0:
+                                voltar = true;
+                                break;
                             case 1:
-                                calcularInvestimento("CDB", contaInvestimento);
+                                System.out.println("Seu saldo é de R$ " + contaInvestimento.getSaldo());
+                                System.out.println("Quanto deseja depositar?: ");
+                                double deposito = scanner.nextDouble();
+                                contaInvestimento.depositar(deposito);
+                                System.out.println("Você depositou R$ " + deposito + " e tem R$ " + contaInvestimento.getSaldo()
+                                        + " de saldo.");
+                                break;
                             case 2:
-                                calcularInvestimento("Tesouro Direto", contaInvestimento);
+                                System.out.println("Seu saldo é de R$ " + contaInvestimento.getSaldo());
+                                System.out.println("Quanto deseja sacar?: ");
+                                double saque = scanner.nextDouble();
+                                contaInvestimento.sacar(saque);
+                                System.out.println("Você sacou R$ " + saque + " e tem R$ " + contaInvestimento.getSaldo()
+                                        + " de saldo.");
+                                break;
                             case 3:
-                                calcularInvestimento("IBV", contaInvestimento);
-                            case 4:
-                                calcularInvestimento("LCI", contaInvestimento);
-                            case 5:
-                                calcularInvestimento("LCI", contaInvestimento);
+                                System.out.println("Escolha os tipos de investimento: ");
+                                System.out.println("1 - CDB.");
+                                System.out.println("2 - Tesouro Direto.");
+                                System.out.println("3 - IBV.");
+                                System.out.println("4 - FCI.");
+                                System.out.println("5 - LCI.");
+
+                                int escolhaInvestimento = scanner.nextInt();
+
+                                switch (escolhaInvestimento) {
+                                    case 1:
+                                        calcularInvestimento("CDB", contaInvestimento);
+                                        break;
+                                    case 2:
+                                        calcularInvestimento("Tesouro Direto", contaInvestimento);
+                                        break;
+                                    case 3:
+                                        calcularInvestimento("IBV", contaInvestimento);
+                                        break;
+                                    case 4:
+                                        calcularInvestimento("FCI", contaInvestimento);
+                                        break;
+                                    case 5:
+                                        calcularInvestimento("LCI", contaInvestimento);
+                                        break;
+                                }
+                                break;
                         }
-                }
+                    }
+                    break;
+            }
         }
     }
 
